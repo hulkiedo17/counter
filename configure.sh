a6dc31fec08ecae48fe636e7c0e8ed57418bf015
Hulkiedo17 <hulkiedo@gmail.com>
2021-04-07 12:23:45 +0300

    Build system update

--- configure.sh
+++ configure.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 
 prefix=/usr/local
-debugsym=true
+debug_flag=true
+silence_flag=true
+
+mkdir -p ./bin 
+cd ./bin
+mkdir -p release debug
+cd ../
 
 for arg in "$@"; do
     case "$arg" in
@@ -9,10 +15,16 @@ for arg in "$@"; do
         prefix=`echo $arg | sed 's/--prefix=/'`
         ;;
     --enable-debug)
-        debugsym=true
+        debug_flag=true
         ;;
     --disable-debug)
-        debugsym=false
+        debug_flag=false
+        ;;
+    --enable-silence)
+        silence_flag=true
+        ;;
+    --disable-silence)
+        silence_flag=false
         ;;
     --help)
         echo "usage: ./configure.sh [options]"
@@ -20,6 +32,8 @@ for arg in "$@"; do
         echo "  --prefix=<path>:    installation path prefix"
         echo "  --enable-debug:     include debug symbols"
         echo "  --disable-debug:    do not include debug symbols"
+        echo "  --enable-silence:   activate silence mode"
+        echo "  --disable-silence:  activate non-silence mode"
         echo "all invalid options are silently ignored"
         ;;
     esac
@@ -30,10 +44,16 @@ echo "generating makefile..."
 echo ".POSIX:" >Makefile
 echo "PREFIX = $prefix" >>Makefile
 
-if $debugsym; then
+if $debug_flag; then
     echo "DEBUG = TRUE" >>Makefile
 else
     echo "DEBUG = FALSE" >>Makefile
+fi
+
+if $silence_flag; then
+    echo "V = TRUE" >>Makefile
+else
+    echo "V = FALSE" >>Makefile
 fi
 
 cat Makefile.in >>Makefile
