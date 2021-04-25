e007d08d21644c5dbe06cf3e920afbb64eac3f0b
Artem Mironov <hulkiedo@gmail.com>
2021-04-25 19:12:06 +0300

    Fixing syntax that weird displayed in github #5

--- src/misc.c
+++ src/misc.c
@@ -9,48 +9,48 @@ static const char* const program_version = "1.0";
 
 void fail(FILE* out, const char *fmt, ...)
 {
-	va_list ap;
+    va_list ap;
 
-	va_start(ap, fmt);
-	vfprintf(out, fmt, ap);
-	va_end(ap);
+    va_start(ap, fmt);
+    vfprintf(out, fmt, ap);
+    va_end(ap);
 }
 
 char* getcw(void)
 {
-	char *path_ptr = NULL;
-	size_t len;
-	char buffer[BUFSIZ];
+    char *path_ptr = NULL;
+    size_t len;
+    char buffer[BUFSIZ];
 
-	if(getcwd(buffer, BUFSIZ) == NULL) {
-		fail(stderr, "getcw(): error taking the path to the current directory (getcwd() failed)\n");
-		return NULL;
-	}
+    if(getcwd(buffer, BUFSIZ) == NULL) {
+        fail(stderr, "getcw(): error taking the path to the current directory (getcwd() failed)\n");
+        return NULL;
+    }
 
-	len = strlen(buffer) + 1;
-	path_ptr = malloc(sizeof(char) * len);
-	if(path_ptr == NULL) {
-		fail(stderr, "getcw(): memory allocation error (malloc() failed)\n");
-		return NULL;
-	}
+    len = strlen(buffer) + 1;
+    path_ptr = malloc(sizeof(char) * len);
+    if(path_ptr == NULL) {
+        fail(stderr, "getcw(): memory allocation error (malloc() failed)\n");
+        return NULL;
+    }
 
-	strncpy(path_ptr, buffer, len);
+    strncpy(path_ptr, buffer, len);
 
-	return path_ptr;
+    return path_ptr;
 }
 
 void help(void) {
-	printf("Usage: counter [options]\n");
-	printf("options:\n");
-	printf("\t(--path) -p [path_name] - specifying another directory for counting lines in files\n");
-	printf("\t(--help) -h - prints this help message\n");
-	printf("\t(--conf) -c - include to count config files\n\n");
+    printf("Usage: counter [options]\n");
+    printf("options:\n");
+    printf("\t(--path) -p [path_name] - specifying another directory for counting lines in files\n");
+    printf("\t(--help) -h - prints this help message\n");
+    printf("\t(--conf) -c - include to count config files\n\n");
 
-	printf("\t(--no-recursion) -R - do not use files in nested(recursive) directories\n");
-	printf("\t(--cur-path)\t -P - just show the current path\n");
-	printf("\t(--detail)\t -d - show exactly which files are in use and the path to them\n");
+    printf("\t(--no-recursion) -R - do not use files in nested(recursive) directories\n");
+    printf("\t(--cur-path)\t -P - just show the current path\n");
+    printf("\t(--detail)\t -d - show exactly which files are in use and the path to them\n");
 }
 
 void version(void) {
-	printf("program version = v%s\n\n", program_version);
+    printf("program version = v%s\n\n", program_version);
 }
